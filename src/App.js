6928f49b96ee3dc58cb2d13c5af0e3a30e498149
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-27 13:58:56 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,38 @@ import "./App.css";
 import Register from "./Component/Register";
 import AuthProvider from "./Context/AuthProvider";
 import firbase from './firebase';
+import Login from "./Component/Login";
+import Dashboard from "./Component/Dashboard";
+import Layout from "./Component/Layout";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 
 function App() {
+
+  const routes= createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout />,
+      children:[
+        {
+          path:"/page",
+          element:<Dashboard />
+        },
+      ]
+    },
+    {
+      path:"/login",
+      element:<Login />
+    },
+    {
+      path:"/signup",
+      element:<Register />
+    },
+  ])
 
   return (
     <>
       <AuthProvider>
-        <Register />
+        <RouterProvider router={routes}/>
       </AuthProvider>
     </>
   );
